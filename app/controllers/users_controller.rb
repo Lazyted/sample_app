8d433769e2646e02e26f7ed5466bc47b0e2733b0
lazyted <lazyted@gmail.com>
2013-05-29 00:10:00 +0300

    Finish user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,22 @@
 class UsersController < ApplicationController
+  
+  def show
+  	@user = User.find(params[:id])
+  end
+
   def new
+  	@user = User.new
+  end
+
+  def create
+  	@user = User.new(params[:user])
+  	if @user.save
+  		# Show a one off "flash" message after succeful registration
+  		flash[:success] = "Welcome to the Sample App!"
+  		# Handle a successful save
+  		redirect_to @user
+  	else
+  		render 'new'
+  	end
   end
 end
